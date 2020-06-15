599012e0948795871f943fa133471ad9da142496
Yulian Lobotskyi <yulian.lobotskyi@globallogic.com>
2020-06-15 10:00:16 +0300

    Added SCSS modeuls and styled components

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
+
+const Button = styled.button`
+    cursor: pointer;
+    background: transparent;
+    font-size: 16px;
+    border-radius: 3px;
+    color: palevioletred;
+    border: 2px solid palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+    transition: 0.5s all ease-out;
+
+    &:hover {
+    background-color: palevioletred;
+    color: white;
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button>Press!</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
